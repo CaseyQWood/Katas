1e010bf38270de2d5a8ae1f8fa970ca837291e41
Casey Wood <caseywood252@gmail.com>
2021-05-10 06:32:40 +0000

    air quality check

---
+++ kata-7.js
@@ -0,0 +1,39 @@
+const checkAir = function (samples, threshold) {
+  let air = 'clean'
+  let howDirty = []
+  // my variables 
+  let percent = samples.length * threshold
+  // calculating the percent allowed
+  for (let i = 0; i < samples.length; i++){
+    if (samples[i] === 'dirty'){
+      howDirty.push(samples[i])
+    //finding how many indexes are dirty
+    }
+  }
+  if (howDirty.length > percent){
+    air = 'dirty'
+  // changing the result of air if the number of dirty's is higher then the threshold amount
+  }
+  return air
+};
+
+
+
+
+
+
+
+  console.log(checkAir(
+    ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+    0.3
+  ));
+  
+  console.log(checkAir(
+    ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+    0.25
+  ));
+  
+  console.log(checkAir(
+    ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+    0.9
+  ))
