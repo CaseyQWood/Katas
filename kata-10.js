f914718194b2cf5fa72edba602920d3b1cbff586
Casey Wood <caseywood252@gmail.com>
2021-05-11 19:21:35 +0000

    leaned up code with comments

--- kata-10.js
+++ kata-10.js
@@ -1,11 +1,15 @@
 const multiplicationTable = function(maxValue) {
-  let answer = '\n'
+  let answer = ''
+  // setting empty variable as a container
   for (let i = 1; i < maxValue + 1; i ++){
-    // let trueStep = i + 1
+  // loop to create the number of rows and number for each row to multiply by
     for (let x = 1; x < maxValue + 1; x++){
-    answer += (i*x) + ''
+    // second loop creates the numbers to mutiply the rows by
+    answer += `${i*x} `
+    // sets the math for each step to give the mutiplication and sets it as a string so each input is not added mathicatically to the last
     }
     answer += '\n'
+    // creates the space after each row is iterated through
   }
   return answer
 };
